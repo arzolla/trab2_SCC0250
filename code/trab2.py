e94847fc661ef8e4ee7c8f1f0ed2d251ca0ef42c
Victor M. Arzolla <victor.arzolla@gmail.com>
2021-06-24 19:03:39 -0300

    Resto do codigo base
    Adicionado resto do código base, precisa consertar o acesso aos arquivos

--- code/trab2.py
+++ code/trab2.py
@@ -11,18 +11,19 @@
 # Importando dependências
 import glfw
 from OpenGL.GL import *
-import OpenGL.GL.shaders
+#import OpenGL.GL.shaders
 import numpy as np
 import math
 import glm
+from PIL import Image
 import sys, os
 sys.path.append(os.path.join(sys.path[0],'sources'))
 
 # Importa módulo com os codigos referentes aos shaders e buffer
-from shader_buffer import *
+import shader_buffer as sb
 # Importa módulo com os codigos referentes aos comandos de teclado
-import key_commands as key_c
-from objects import *
+#import key_commands as cmd
+#import objects as obj
 
 
 # inicializa o GLFW
@@ -42,7 +43,7 @@ glfw.make_context_current(window)
 
 
 # Função para rodar shaders, retorna variavel programa principal
-program = run_shader()
+program = sb.run_shader()
 
 
 #########################################
@@ -50,30 +51,151 @@ program = run_shader()
 
 
 # Instanciação dos vértices
-mato = gen_mato()
-rua = gen_rua()
-carro = Carro()
-nuvem0 = Nuvem(0)
-nuvem1 = Nuvem(1)
-nuvem2 = Nuvem(2)
-nuvem3 = Nuvem(3)
-nuvem4 = Nuvem(4)
-nuvem5 = Nuvem(5)
-sol = Sol()
-faixa0=Faixas(0)
 
-faixa = []
-for i in range(10):
-    faixa.append(Faixas(i))
+def load_model_from_file(filename):
+    """Loads a Wavefront OBJ file. """
+    objects = {}
+    vertices = []
+    texture_coords = []
+    faces = []
+
+    material = None
+
+    # abre o arquivo obj para leitura
+    for line in open(filename, "r"): ## para cada linha do arquivo .obj
+        if line.startswith('#'): continue ## ignora comentarios
+        values = line.split() # quebra a linha por espaço
+        if not values: continue
 
 
-lista = [nuvem0.coord,nuvem1.coord,mato,rua,carro.corpo,carro.roda1,carro.roda2,
-carro.calota1,carro.calota2,carro.aro1,carro.aro2,sol.coord,sol.raios,carro.corpo2,
-faixa[0].coord,faixa[1].coord,faixa[2].coord,faixa[3].coord,faixa[4].coord,
-faixa[5].coord,faixa[6].coord,faixa[7].coord,faixa[8].coord,faixa[9].coord
-]
-# Concatena os vertices para enviar ao buffer
-vertices = np.concatenate((lista))
+        ### recuperando vertices
+        if values[0] == 'v':
+            vertices.append(values[1:4])
+
+
+        ### recuperando coordenadas de textura
+        elif values[0] == 'vt':
+            texture_coords.append(values[1:3])
+
+        ### recuperando faces 
+        elif values[0] in ('usemtl', 'usemat'):
+            material = values[1]
+        elif values[0] == 'f':
+            face = []
+            face_texture = []
+            for v in values[1:]:
+                w = v.split('/')
+                face.append(int(w[0]))
+                if len(w) >= 2 and len(w[1]) > 0:
+                    face_texture.append(int(w[1]))
+                else:
+                    face_texture.append(0)
+
+            faces.append((face, face_texture, material))
+
+    model = {}
+    model['vertices'] = vertices
+    model['texture'] = texture_coords
+    model['faces'] = faces
+
+    return model
+
+
+glEnable(GL_TEXTURE_2D)
+qtd_texturas = 10
+textures = glGenTextures(qtd_texturas)
+
+def load_texture_from_file(texture_id, img_textura):
+    glBindTexture(GL_TEXTURE_2D, texture_id)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    img = Image.open(img_textura)
+    img_width = img.size[0]
+    img_height = img.size[1]
+    image_data = img.tobytes("raw", "RGB", 0, -1)
+    #image_data = np.array(list(img.getdata()), np.uint8)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img_width, img_height, 0, GL_RGB, GL_UNSIGNED_BYTE, image_data)
+
+
+vertices_list = []    
+textures_coord_list = []
+
+modelo = load_model_from_file(os.path.join(sys.path[0],'sources','caixa','caixa.obj'))
+
+### inserindo vertices do modelo no vetor de vertices
+print('Processando modelo cube.obj. Vertice inicial:',len(vertices_list))
+for face in modelo['faces']:
+    for vertice_id in face[0]:
+        vertices_list.append( modelo['vertices'][vertice_id-1] )
+    for texture_id in face[1]:
+        textures_coord_list.append( modelo['texture'][texture_id-1] )
+print('Processando modelo cube.obj. Vertice final:',len(vertices_list))
+
+### inserindo coordenadas de textura do modelo no vetor de texturas
+
+
+### carregando textura equivalente e definindo um id (buffer): use um id por textura!
+load_texture_from_file(0,os.path.join(sys.path[0],'sources','caixa','caixa2.jpg'))
+
+
+modelo = load_model_from_file('terreno/terreno2.obj')
+
+### inserindo vertices do modelo no vetor de vertices
+print('Processando modelo terreno.obj. Vertice inicial:',len(vertices_list))
+for face in modelo['faces']:
+    for vertice_id in face[0]:
+        vertices_list.append( modelo['vertices'][vertice_id-1] )
+    for texture_id in face[1]:
+        textures_coord_list.append( modelo['texture'][texture_id-1] )
+print('Processando modelo terreno.obj. Vertice final:',len(vertices_list))
+
+### inserindo coordenadas de textura do modelo no vetor de texturas
+
+
+### carregando textura equivalente e definindo um id (buffer): use um id por textura!
+load_texture_from_file(1,'terreno/pedra.jpg')
+
+modelo = load_model_from_file('casa/casa.obj')
+
+### inserindo vertices do modelo no vetor de vertices
+print('Processando modelo casa.obj. Vertice inicial:',len(vertices_list))
+for face in modelo['faces']:
+    for vertice_id in face[0]:
+        vertices_list.append( modelo['vertices'][vertice_id-1] )
+    for texture_id in face[1]:
+        textures_coord_list.append( modelo['texture'][texture_id-1] )
+print('Processando modelo casa.obj. Vertice final:',len(vertices_list))
+
+### inserindo coordenadas de textura do modelo no vetor de texturas
+
+
+### carregando textura equivalente e definindo um id (buffer): use um id por textura!
+load_texture_from_file(2,'casa/casa.jpg')
+
+
+modelo = load_model_from_file('monstro/monstro.obj')
+
+### inserindo vertices do modelo no vetor de vertices
+print('Processando modelo monstro.obj. Vertice inicial:',len(vertices_list))
+for face in modelo['faces']:
+    for vertice_id in face[0]:
+        vertices_list.append( modelo['vertices'][vertice_id-1] )
+    for texture_id in face[1]:
+        textures_coord_list.append( modelo['texture'][texture_id-1] )
+print('Processando modelo monstro.obj. Vertice final:',len(vertices_list))
+
+### inserindo coordenadas de textura do modelo no vetor de texturas
+
+
+### carregando textura equivalente e definindo um id (buffer): use um id por textura!
+load_texture_from_file(3,'monstro/monstro.jpg')
+
+
+
+
+
 
 
 
@@ -81,117 +203,303 @@ vertices = np.concatenate((lista))
 #########################################
 
 
-# Roda os buffers, recebe programa e os vertices e retorna loc e loc_color
-loc, loc_color = run_buffer(program, vertices)
+# Roda buffer de vertice
+vertices = np.zeros(len(vertices_list), [("position", np.float32, 3)])
+vertices['position'] = vertices_list
+
+loc_vertices = sb.vertex_buffer(program, vertices)
+
+
+# Roda buffer de textura
+textures = np.zeros(len(textures_coord_list), [("position", np.float32, 2)]) # duas coordenadas
+textures['position'] = textures_coord_list
+
+loc_texture_coord = sb.texture_buffer(program,textures)
+
+
+#########################################
+#########################################
+
+
+
+def desenha_caixa():
+    
+    
+    # aplica a matriz model
+    
+    # rotacao
+    angle = 0.0;
+    r_x = 0.0; r_y = 0.0; r_z = 1.0;
+    
+    # translacao
+    t_x = 0.0; t_y = 0.0; t_z = 15.0;
+    
+    # escala
+    s_x = 1.0; s_y = 1.0; s_z = 1.0;
+    
+    mat_model = model(angle, r_x, r_y, r_z, t_x, t_y, t_z, s_x, s_y, s_z)
+    loc_model = glGetUniformLocation(program, "model")
+    glUniformMatrix4fv(loc_model, 1, GL_TRUE, mat_model)
+       
+    #define id da textura do modelo
+    glBindTexture(GL_TEXTURE_2D, 0)
+    
+    
+    # desenha o modelo
+    glDrawArrays(GL_TRIANGLES, 0, 36) ## renderizando
+    
+
+def desenha_terreno():
+    
+    
+    # aplica a matriz model
+    
+    # rotacao
+    angle = 0.0;
+    r_x = 0.0; r_y = 0.0; r_z = 1.0;
+    
+    # translacao
+    t_x = 0.0; t_y = -1.01; t_z = 0.0;
+    
+    # escala
+    s_x = 20.0; s_y = 20.0; s_z = 20.0;
+    
+    mat_model = model(angle, r_x, r_y, r_z, t_x, t_y, t_z, s_x, s_y, s_z)
+    loc_model = glGetUniformLocation(program, "model")
+    glUniformMatrix4fv(loc_model, 1, GL_TRUE, mat_model)
+       
+    #define id da textura do modelo
+    glBindTexture(GL_TEXTURE_2D, 1)
+    
+    
+    # desenha o modelo
+    glDrawArrays(GL_TRIANGLES, 36, 42-36) ## renderizando
+
+
+
+def desenha_casa():
+    
+    
+    # aplica a matriz model
+    
+    # rotacao
+    angle = 0.0;
+    r_x = 0.0; r_y = 0.0; r_z = 1.0;
+    
+    # translacao
+    t_x = 0.0; t_y = -1.0; t_z = 0.0;
+    
+    # escala
+    s_x = 1.0; s_y = 1.0; s_z = 1.0;
+    
+    mat_model = model(angle, r_x, r_y, r_z, t_x, t_y, t_z, s_x, s_y, s_z)
+    loc_model = glGetUniformLocation(program, "model")
+    glUniformMatrix4fv(loc_model, 1, GL_TRUE, mat_model)
+       
+    #define id da textura do modelo
+    glBindTexture(GL_TEXTURE_2D, 2)
+    
+    
+    # desenha o modelo
+    glDrawArrays(GL_TRIANGLES, 42, 1476-42) ## renderizando
+
+
+def desenha_monstro(rotacao_inc):
+    
+    
+    # aplica a matriz model
+    
+    # rotacao
+    angle = rotacao_inc;
+    r_x = 0.0; r_y = 1.0; r_z = 0.0;
+    
+    # translacao
+    t_x = 0.0; t_y = -1.0; t_z = 0.0;
+    
+    # escala
+    s_x = 1.0; s_y = 1.0; s_z = 1.0;
+    
+    mat_model = model(angle, r_x, r_y, r_z, t_x, t_y, t_z, s_x, s_y, s_z)
+    loc_model = glGetUniformLocation(program, "model")
+    glUniformMatrix4fv(loc_model, 1, GL_TRUE, mat_model)
+       
+    #define id da textura do modelo
+    glBindTexture(GL_TEXTURE_2D, 3)
+    
+    
+    # desenha o modelo
+    glDrawArrays(GL_TRIANGLES, 1476, 7584-1476) ## renderizando
+
+
+#########################################
+#########################################
+
+cameraPos   = glm.vec3(0.0,  0.0,  1.0);
+cameraFront = glm.vec3(0.0,  0.0, -1.0);
+cameraUp    = glm.vec3(0.0,  1.0,  0.0);
+
+
+polygonal_mode = False
+
+def key_event(window,key,scancode,action,mods):
+    global cameraPos, cameraFront, cameraUp, polygonal_mode
+    
+    cameraSpeed = 0.2
+    if key == 87 and (action==1 or action==2): # tecla W
+        cameraPos += cameraSpeed * cameraFront
+    
+    if key == 83 and (action==1 or action==2): # tecla S
+        cameraPos -= cameraSpeed * cameraFront
+    
+    if key == 65 and (action==1 or action==2): # tecla A
+        cameraPos -= glm.normalize(glm.cross(cameraFront, cameraUp)) * cameraSpeed
+        
+    if key == 68 and (action==1 or action==2): # tecla D
+        cameraPos += glm.normalize(glm.cross(cameraFront, cameraUp)) * cameraSpeed
+        
+    if key == 80 and action==1 and polygonal_mode==True:
+        polygonal_mode=False
+    else:
+        if key == 80 and action==1 and polygonal_mode==False:
+            polygonal_mode=True
+        
+        
+        
+firstMouse = True
+yaw = -90.0 
+pitch = 0.0
+lastX =  largura/2
+lastY =  altura/2
+
+def mouse_event(window, xpos, ypos):
+    global firstMouse, cameraFront, yaw, pitch, lastX, lastY
+    if firstMouse:
+        lastX = xpos
+        lastY = ypos
+        firstMouse = False
+
+    xoffset = xpos - lastX
+    yoffset = lastY - ypos
+    lastX = xpos
+    lastY = ypos
+
+    sensitivity = 0.3 
+    xoffset *= sensitivity
+    yoffset *= sensitivity
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    
+    if pitch >= 90.0: pitch = 90.0
+    if pitch <= -90.0: pitch = -90.0
+
+    front = glm.vec3()
+    front.x = math.cos(glm.radians(yaw)) * math.cos(glm.radians(pitch))
+    front.y = math.sin(glm.radians(pitch))
+    front.z = math.sin(glm.radians(yaw)) * math.cos(glm.radians(pitch))
+    cameraFront = glm.normalize(front)
+
+
+    
+glfw.set_key_callback(window,key_event)
+glfw.set_cursor_pos_callback(window, mouse_event)
+
+
+#########################################
+#########################################
+
+def model(angle, r_x, r_y, r_z, t_x, t_y, t_z, s_x, s_y, s_z):
+    
+    angle = math.radians(angle)
+    
+    matrix_transform = glm.mat4(1.0) # instanciando uma matriz identidade
+
+    
+    # aplicando translacao
+    matrix_transform = glm.translate(matrix_transform, glm.vec3(t_x, t_y, t_z))    
+    
+    # aplicando rotacao
+    matrix_transform = glm.rotate(matrix_transform, angle, glm.vec3(r_x, r_y, r_z))
+    
+    # aplicando escala
+    matrix_transform = glm.scale(matrix_transform, glm.vec3(s_x, s_y, s_z))
+    
+    matrix_transform = np.array(matrix_transform).T # pegando a transposta da matriz (glm trabalha com ela invertida)
+    
+    return matrix_transform
+
+def view():
+    global cameraPos, cameraFront, cameraUp
+    mat_view = glm.lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+    mat_view = np.array(mat_view)
+    return mat_view
+
+def projection():
+    global altura, largura
+    # perspective parameters: fovy, aspect, near, far
+    mat_projection = glm.perspective(glm.radians(45.0), largura/altura, 0.1, 1000.0)
+    mat_projection = np.array(mat_projection)    
+    return mat_projection
+
+
+
+#########################################
+#########################################
+
+
 
 # Mostra a janela
 glfw.show_window(window)
+glfw.set_cursor_pos(window, lastX, lastY)
 
-# Ativa os comandos do teclado
-key_c.run_key_commands(window)
+glEnable(GL_DEPTH_TEST) ### importante para 3D
+   
 
-
-# Envia para classes as variáveis de programa, loc e loc_color
-Nuvem.program = program
-Nuvem.loc = loc
-Nuvem.loc_color = loc_color
-
-Carro.program = program
-Carro.loc = loc
-Carro.loc_color = loc_color
-
-Sol.program = program
-Sol.loc = loc
-Sol.loc_color = loc_color
-
-Faixas.program = program
-Faixas.loc = loc
-Faixas.loc_color = loc_color
+rotacao_inc = 0
 
 
-# inicialização de variaveis
-carro_x = 0
-carro_y = 0
-ceu_x = 0
-ceu_y = 0
-ceu_alt_x = 0
-escala_sol = 1
-#key_c.program = program
-cont_faixa = 0
+#########################################
+#########################################
+
 
 # Loop principal
 while not glfw.window_should_close(window):
 
-    #
-    glfw.poll_events()
+    glfw.poll_events() 
+    
+    
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    
+    glClearColor(1.0, 1.0, 1.0, 1.0)
+    
+    if polygonal_mode==True:
+        glPolygonMode(GL_FRONT_AND_BACK,GL_LINE)
+    if polygonal_mode==False:
+        glPolygonMode(GL_FRONT_AND_BACK,GL_FILL)
+    
+    
 
+    desenha_caixa()   
+    desenha_terreno()
+    desenha_casa()
+    
+    rotacao_inc += 0.1
+    desenha_monstro(rotacao_inc)
+  
 
+    
+    mat_view = view()
+    loc_view = glGetUniformLocation(program, "view")
+    glUniformMatrix4fv(loc_view, 1, GL_FALSE, mat_view)
 
+    mat_projection = projection()
+    loc_projection = glGetUniformLocation(program, "projection")
+    glUniformMatrix4fv(loc_projection, 1, GL_FALSE, mat_projection)    
+    
+    
 
-    # comparações para restringir os limites dos comandos
-    if escala_sol < 2:
-        escala_sol = escala_sol + 0.001*key_c.f_sol_s
-
-    ceu_x = ceu_x - 0.0005
-    ceu_alt_x = ceu_alt_x - 0.015*key_c.f_ceu_x
-    ceu_x = ceu_x - 0.001*key_c.f_ceu_x # SPACE
-    ceu_y = ceu_y + 0.0005
-
-    if  carro_y > 0:
-        carro_y = carro_y - (0.01*key_c.f_carro_yb)*key_c.f_ceu_x # S, depende de SPACE apertado
-
-    if carro_y < 0.4:
-        carro_y = carro_y + (0.01*key_c.f_carro_ya)*key_c.f_ceu_x # W, depende de SPACE apertado
-
-    if  carro_x > 0:
-        carro_x = carro_x - 0.01*key_c.f_carro_xa # A
-    if carro_x < 1.4:
-        carro_x = carro_x + 0.01*key_c.f_carro_xb # D
-
-
-
-    glClear(GL_COLOR_BUFFER_BIT)
-    glClearColor(*cor_bg, 1.0)
-
-    # matriz identidade
-    mat_id = np.array([             1.0, 0.0, 0.0, 0.0,
-                                    0.0, 1.0, 0.0, 0.0,
-                                    0.0, 0.0, 1.0, 0.0,
-                                    0.0, 0.0, 0.0, 1.0], np.float32)
-
-
-    # Desenha o sol
-    sol.move(lista,0,0,0,ceu_y,escala_sol)
-
-    # Cenário
-    loc = glGetUniformLocation(program, "mat_transformation")
-    glUniformMatrix4fv(loc, 1, GL_TRUE, mat_id)
-
-
-   # Desenha mato
-    insert_obj(loc_color,lista,mato,0,cor_mato)
-
-    # Desenha rua
-    insert_obj(loc_color,lista,rua,0,cor_rua)
-
-
-
-    # Dexenha todas as faixas
-    for i in range(len(faixa)):
-
-        faixa[i].move(lista,cont_faixa,0,carro_x,ceu_alt_x)
-        cont_faixa = cont_faixa+0.8
-    cont_faixa = 0
-
-
-
-    # insere o carro
-    carro.move(lista,-0.7,-0.630 ,carro_x,carro_y,ceu_alt_x)
-    # insere nuvem
-    nuvem0.move(lista,0.3,0.3,ceu_x)
-    nuvem1.move(lista,-0.6,0.6,ceu_x)
-
-
+    
     glfw.swap_buffers(window)
 
 glfw.terminate()
